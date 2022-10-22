9db9b59760e96991c1052632e58db01c009bad91
sukruhasta95 <sukruhasta95@gmail.com>
2022-10-22 16:42:51 +0300

    12. ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofact.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.Concrete.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,13 +24,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             //iş kodları buraya yazılır.
-            // koşullar vs.
-            if (product.ProductName.Length < 2)
-                return new ErrorResult(Messages.ProductNameInvalid);
-
+            
 
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,8 +30,6 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
